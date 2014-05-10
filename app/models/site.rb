3c940f60586857595595bb3973bda9b5a96d281b
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-05-10 12:45:03 +0900

    Add validates

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,6 @@
 class Site < ActiveRecord::Base
   belongs_to :user
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :s3_access_key, length: { maximum: 100 }
+  validates :s3_secret_key, length: { maximum: 100 }
 end
