ff6d0030f96b1ca469d3bf2a932077f376e62f46
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-05-10 16:14:00 +0900

    [WIP] creating previewing

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,10 @@ class Page < ActiveRecord::Base
   validates :template, presence: true, length: { maximum: 100 }
   validates :path, presence: true, length: { maximum: 100 }
   validates :data, length: { maximum: 5000 }
+
+  def render(context)
+    @data = JSON::parse(data)
+    tilt = Tilt.new(Rails.root.join('themes', site.theme, 'templates', template).to_s)
+    context.render text: tilt.render(context, page: self)
+  end
 end
