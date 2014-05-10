fe7c4e7179f3e8a64aec7614e1f1730ef041cd75
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-05-10 15:09:16 +0900

    Enable to modify theme

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,7 @@
 class Site < ActiveRecord::Base
   belongs_to :user
   validates :name, presence: true, length: { maximum: 50 }
+  validates :theme, presence: true, length: { maximum: 100 }
   validates :s3_access_key, length: { maximum: 100 }
   validates :s3_secret_key, length: { maximum: 100 }
 end
